31eba1e2e2415c7bb8f2ccef332579fdeeedbca8
Grant Heath <heathgr@gmail.com>
2017-04-04 13:32:21 -0600

    Added immutable prop types.

--- client/src/components/Main.jsx
+++ client/src/components/Main.jsx
@@ -1,14 +1,19 @@
 import React, { PropTypes } from 'react';
+import ImmutablePropTypes from 'react-immutable-proptypes';
 
 const Main = ({ matrix }) => (
   <div>
     <div>{'Welcome to Matrix Traversal from main component!!!'}</div>
     <div>{matrix.get('cells').size}</div>
+    <div>{'noice!!!'}</div>
   </div>
 );
 
-export default Main;
-
 Main.propTypes = {
-  matrix: PropTypes.object.isRequired,
+  matrix: ImmutablePropTypes.mapContains({
+    cells: ImmutablePropTypes.listOf(PropTypes.number).isRequired,
+    columnCount: PropTypes.number.isRequired,
+  }).isRequired,
 };
+
+export default Main;
