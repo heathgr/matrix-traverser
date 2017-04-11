bc2433262521ddf31b6f23c395dc3cdde5e9a753
Grant Heath <heathgr@gmail.com>
2017-04-10 23:06:04 -0600

    Now using nyc for client test coverage reports.

--- client/scripts/test.sh
+++ client/scripts/test.sh
@@ -1,6 +1,6 @@
 export CURRENT_NODE_ENV=$NODE_ENV
 export NODE_ENV='test'
 echo "\nRUNNING YOUR TESTS LIKE A BOSS!!!\n"
-find ./spec -type f -name "*.spec.js" -o -name "*.spec.jsx" | xargs node_modules/.bin/_mocha --require jsdom.config.js --compilers js:babel-core/register
+node_modules/.bin/_mocha $(find ./spec -type f -name "*.spec.js" -o -name "*.spec.jsx") --require jsdom.config.js --compilers js:babel-core/register
 export NODE_ENV=$CURRENT_NODE_ENV
 exit 0;
--- client/scripts/testCoverage.sh
+++
@@ -1,8 +0,0 @@
-export CURRENT_NODE_ENV=$NODE_ENV
-export NODE_ENV='test'
-echo "\nGENERATING MOCHA COVERAGE REPORTS LIKE A BOSS:\n"
-find ./spec -type f -name "*.spec.js" -o -name "*.spec.jsx" | xargs node_modules/.bin/babel-istanbul cover node_modules/.bin/_mocha -x client/spec/**/*.spec.js -- --require jsdom.config.js
-echo "\n"
-export NODE_ENV=$CURRENT_NODE_ENV
-open ./coverage/lcov-report/index.html
-exit 0;
