ff69c0fe7e3957bb7b56d37572d68b7e022ba31e
Grant Heath <heathgr@gmail.com>
2017-08-04 00:48:07 -0600

    Completed an unfinished test for the <Modal /> component.

--- client/spec/components/Modal.spec.jsx
+++ client/spec/components/Modal.spec.jsx
@@ -1,3 +1,19 @@
+import chai, { expect } from 'chai';
+import chaiEnzyme from 'chai-enzyme';
+import { mount } from 'enzyme';
+import React from 'react';
+import { spy } from 'sinon';
+import Modal from '../../src/components/Modal';
+
 describe('<Modal />', () => {
-  xit('Should be awesome...');
+  chai.use(chaiEnzyme());
+
+  const TestChild = () => <div>test child</div>;
+  const testWrapper = mount(
+    <Modal>
+      <TestChild />
+    </Modal>
+  );
+
+  it('Should render correctly render its children.', () => expect(testWrapper.find(TestChild)).to.exist);
 });
