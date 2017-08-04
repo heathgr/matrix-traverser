33ca219186600aba81f106064262119d56bb7e59
Grant Heath <heathgr@gmail.com>
2017-08-04 01:23:32 -0600

    Completed unfished tests for the <Introduction /> component.

--- client/spec/components/Introduction.spec.jsx
+++ client/spec/components/Introduction.spec.jsx
@@ -1,5 +1,21 @@
-describe('<Introduction />', () => {
-  xit('should be nice.', () => {
+import chai, { expect } from 'chai';
+import chaiEnzyme from 'chai-enzyme';
+import { mount } from 'enzyme';
+import React from 'react';
+import { spy } from 'sinon';
+import Introduction from '../../src/components/Introduction';
 
+describe('<Introduction />', () => {
+  chai.use(chaiEnzyme());
+
+  const onToggleIntroductionUISpy = spy();
+  const testWrapper = mount(
+    <Introduction onToggleIntroductionUI={onToggleIntroductionUISpy} />
+  );
+
+  it('Should have a button the calls the onToggleIntroductionUI function when clicked.', () => {
+    testWrapper.find('button').simulate('click');
+
+    expect(onToggleIntroductionUISpy.calledOnce).to.equal(true);
   });
 });
