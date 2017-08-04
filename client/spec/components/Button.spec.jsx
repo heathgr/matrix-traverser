d15b483fd260285b897d73709a9a5303046bf27b
Grant Heath <heathgr@gmail.com>
2017-08-04 00:27:32 -0600

    Fixed unfinished test for <Button /> component.

--- client/spec/components/Button.spec.jsx
+++ client/spec/components/Button.spec.jsx
@@ -1,3 +1,31 @@
+import chai, { expect } from 'chai';
+import chaiEnzyme from 'chai-enzyme';
+import { Map } from 'immutable';
+import { mount } from 'enzyme';
+import React from 'react';
+import { spy } from 'sinon';
+import Button from '../../src/components/Button';
+
 describe('<Button />', () => {
-  xit('should be awesome!!');
+  chai.use(chaiEnzyme());
+
+  const onClickSpy = spy();
+  const TestChild = () => <div>test child</div>;
+  const testWrapper = mount(
+    <Button
+      onClick={onClickSpy}
+    >
+      <TestChild />
+    </Button>
+  );
+
+  it('Should be a wrapper for a button.', () => expect(testWrapper.find('button')).to.exist);
+
+  it('Should call the onClick callback.', () => {
+    testWrapper.simulate('click');
+
+    expect(onClickSpy.calledOnce).to.equal(true);
+  });
+
+  it('Should correctly render children.', () => expect(testWrapper.find(TestChild)).to.exist);
 });
