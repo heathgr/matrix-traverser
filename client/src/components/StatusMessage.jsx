36fb9888c90b7afd285eaf34a5bfd7da785fca56
Grant Heath <heathgr@gmail.com>
2017-08-15 15:47:29 -0600

    Got rid of the transitioner component and switched to react-transition-group instead.

--- client/src/components/StatusMessage.jsx
+++ client/src/components/StatusMessage.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import Style from 'style-it';
 import PropTypes from 'prop-types';
+import TransitionGroup from 'react-transition-group/TransitionGroup';
+import Transition from 'react-transition-group/Transition';
 import ImmutablePropTypes from 'react-immutable-proptypes';
 import {
   PRIMARY_BORDER_COLOR,
@@ -9,8 +11,30 @@ import {
   LOADING_MESSAGE,
   LOADED_MESSAGE,
 } from '../constants/statusMessageTypes';
-import Transitioner from './Transitioner';
 import PureImmutable from '../helpers/hocs/PureImmutable';
+
+
+const fadeDuration = 500;
+const fadeClassname = {
+  entering: 'entering',
+  entered: 'entered',
+  exiting: 'exiting',
+  exited: 'exited',
+};
+
+const Fader = ({ in: inProp, out: outProp, children }) => (
+  <Transition in={inProp} out={outProp} timeout={fadeDuration} unmountOnExit>
+    {
+      (state) => {
+        return (<div className={`transitionItem ${fadeClassname[state]}`}>
+          {
+            children
+          }
+        </div>);
+      }
+    }
+  </Transition>
+);
 
 const StatusMessage = ({
   messageType,
@@ -18,6 +42,16 @@ const StatusMessage = ({
   error,
 }) => Style.it(
   `
+    @keyframes transitionInKeyframes {
+      0% {opacity: 0; }
+      100% {opacity: 1; }
+    }
+
+    @keyframes transitionOutKeyframes {
+      0% {opacity: 1; }
+      100% {opacity: 0; }
+    }
+
     .statusMessage {
       color: ${PRIMARY_BORDER_COLOR};
       margin: 8px;
@@ -25,28 +59,65 @@ const StatusMessage = ({
       height: 13px;
       font-size: 13px;
     }
+
+    .transitionItem {
+      position: absolute;
+      left: 0px;
+      right: 0px;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+    }
+
+    .entering {
+      animation: 500ms ease transitionInKeyframes;
+    }
+
+    .entered {
+      opacity: 1;
+    }
+
+    .exiting {
+      animation: 500ms ease transitionOutKeyframes;
+    }
+
+    .exited {
+      opacity: 0;
+    }
   `,
   (
     <div className='statusMessage'>
-      <Transitioner>
+      <TransitionGroup>
         {
           (
             () => {
               switch (messageType) {
                 case LOADING_MESSAGE: {
-                  return <div>Loading</div>;
+                  return (
+                    <Fader key={0}>
+                      Loading
+                    </Fader>
+                  );
                 }
                 case LOADED_MESSAGE: {
-                  return <div>{`Found ${solutions.size} solutions with a length of ${solutions.get(0).size}.`}</div>;
+                  return (
+                    <Fader key={1}>
+                      {`Found ${solutions.size} solutions with a length of ${solutions.get(0).size}.`}
+                    </Fader>
+                  );
                 }
                 default: {
-                  return <div>{error}</div>;
+                  return (
+                    <Fader key={2}>
+                      {error}
+                    </Fader>
+                  );
                 }
               }
             }
           )()
         }
-      </Transitioner>
+      </TransitionGroup>
     </div>
   )
 );
