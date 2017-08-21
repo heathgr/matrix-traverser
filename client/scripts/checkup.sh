eec0c47009b6d432b4e8293e4b3ae6a1efc61d37
Grant Heath <heathgr@gmail.com>
2017-08-21 16:43:34 -0600

    Fixed a bug where not all tests where run when using the checkup script.

--- client/scripts/checkup.sh
+++ client/scripts/checkup.sh
@@ -4,7 +4,7 @@ echo "\nRUNNING YOUR CODE CHECKUP LIKE A BOSS!!!\n"
 echo "ESLINT:\n"
 node_modules/.bin/eslint ./src ./spec
 echo "\nMOCHA:\n"
-node_modules/.bin/_mocha --require jsdom.config.js --compilers js:babel-core/register --recursive spec/**/*.spec.js
+./scripts/test.sh
 echo "\n"
 export NODE_ENV=$CURRENT_NODE_ENV
 exit 0;
