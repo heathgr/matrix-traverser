207ac47320f3726675a72a5d957259f06674fead
Grant Heath <heathgr@gmail.com>
2017-08-20 18:13:28 -0600

    Made sure that the build script worked.

--- client/scripts/build.sh
+++ client/scripts/build.sh
@@ -1,4 +1,3 @@
-node_modules/.bin/eslint ./src ./spec &&
-node_modules/.bin/_mocha --require jsdom.config.js --compilers js:babel-core/register --recursive spec/**/*.spec.js &&
+./scripts/checkup.sh &&
 node_modules/.bin/webpack --config webpack.production.config.js -p;
 exit 0;
